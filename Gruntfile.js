61270dfd170e5029870cf5075e02a2df4c76c28c
Jacob Rief <jacob.rief@gmail.com>
2013-03-08 12:25:13 +0100

    Initial revision

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'lib/*.js'],
+      options: {
+        curly: false,
+        browser: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        expr: true,
+        node: true,
+        globals: {
+          exports: true,
+          angular: false,
+          $: false
+        }
+      }
+    },
+    testacular: {
+      test: {
+        options: {
+          reporters: ['dots'],
+          browsers: ['Firefox', 'Chrome', 'Safari', 'Opera'],
+          singleRun: true
+        }
+      },
+      server: {
+        options: {
+          browsers: ['PhantomJS'],
+          singleRun: false
+        }
+      },
+      options: {
+        configFile: 'test/testacular.conf.js'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['test']);
+
+  // Test tasks.
+  grunt.registerTask('test', ['jshint', 'testacular:test']);
+  grunt.registerTask('test-server', ['testacular:server']);
+
+  // Build task.
+  grunt.registerTask('build', ['test', 'concat', 'uglify']);
+
+  // Provides the "testacular" task.
+  grunt.registerMultiTask('testacular', 'Starts up a testacular server.', function() {
+    var done = this.async();
+    require('testacular').server.start(this.options(), function(code) {
+      done(code === 0);
+    });
+  });
+};
--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-angular-retina
-==============
+# angular-retina
 
-Add support for Retina displays when using element attribute "ng-src"
+Replace AngularJS directive 'ng-src' by a version which supports Retina displays
+
+## Quick start
+### Download
+
+[min]: https://raw.github.com/jrief/angular-retina/master/dist/angular-retina.min.js
+[max]: https://raw.github.com/jrief/angular-retina/master/dist/angular-retina.js
+
++ Into your html include the required libraries:
+
+>
+``` html
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.1.3/angular.min.js"></script>
+<script src="/path/to/your/javascript-files/angular-retina.min.js"></script>
+```
+
+### Add to your project
+
+>
+```javascript
+angular.module('MyAwesomeModule', [...other dependencies..., 'ngRetina']);
+```
+
+use it in your HTML-code as you would use the common Angular directive
+[ngSrc](http://docs.angularjs.org/api/ng.directive:ngSrc):
+
+>
+```html
+<img ng-src="/path/to/any/image.png" width="100" height="100">
+```
+
+## On the server
+Applications supporting Retina displays should include two separate files for
+each image resource. One file provides a standard-resolution version of a given
+image, and the second provides a high-resolution version of the same image.
+The naming conventions for each pair of image files is as follows:
++ Standard: <image_name>.<filename_extension>
++ High resolution: <image_name>@2x.<filename_extension>
+
+If the browser runs on a high-resolution display, and if the referenced image
+is available in high-resolution, the corresponding ```<img ng-src="...">``` tag
+is interpreted, such that the image in high-resolution is referenced.
+This module also rewrites ```<img ng-src="...">``` tags, which contain a static
+image url, ie. one without any mark-up directives.
+
+Note that when using this module, adding the element attributes ```width="..."```
+ and ```height="..."``` becomes mandatory, as the displayed high-resolution image
+otherwise gets scaled to the double size.
+
+## Release History
+0.1.0 - initial revision
+
+## License
+Copyright (c) 2013 Jacob Rief  
+Licensed under the MIT license.
